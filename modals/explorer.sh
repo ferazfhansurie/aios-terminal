95f15d0f28c2c3312bb3d7c31e5a5e1e53acf929
Firaz <firaz@adletic.dev>
2026-05-04 18:48:09 +0800

    fix: alacritty cwd → home, ⌘I unbind tpm clash, harden explorer popup

--- modals/explorer.sh
+++ modals/explorer.sh
@@ -3,5 +3,10 @@
 
 set -eu
 
+export PATH="/opt/homebrew/bin:/usr/local/bin:$HOME/.local/bin:$PATH"
+
 START_DIR="${1:-$PWD}"
-exec yazi "$START_DIR"
+[[ -d "$START_DIR" ]] || START_DIR="$HOME"
+
+LOG="${TMPDIR:-/tmp}/yazi-popup.log"
+exec yazi "$START_DIR" 2>"$LOG"
